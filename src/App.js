c09d22159ce675cda141b05e1cdb46f09814e7cc
SaikatRaj <saikatrajgaria@gmail.com>
2022-08-14 12:25:15 +0530

    changed to if else statement

--- src/App.js
+++ src/App.js
@@ -47,27 +47,35 @@ class App extends Component {
             cursor: 'pointer'
         }
 
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                <Person 
+                    name={this.state.persons[0].name} 
+                    age={this.state.persons[0].age} />
+                <Person 
+                    name={this.state.persons[1].name} 
+                    age={this.state.persons[1].age} 
+                    click={this.switchNameHandler.bind(this, 'Max!')}
+                    changed={this.nameChangedHandler} />
+                <Person 
+                    name={this.state.persons[2].name} 
+                    age={this.state.persons[2].age} />
+            </div>
+            );
+        }
+
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <button 
                     style={style} 
-                    onClick={this.togglePersonsHandler}>Switch Name</button>
-                {   this.state.showPersons ?
-                    <div>
-                        <Person 
-                            name={this.state.persons[0].name} 
-                            age={this.state.persons[0].age} />
-                        <Person 
-                            name={this.state.persons[1].name} 
-                            age={this.state.persons[1].age} 
-                            click={this.switchNameHandler.bind(this, 'Max!')}
-                            changed={this.nameChangedHandler} />
-                        <Person 
-                            name={this.state.persons[2].name} 
-                            age={this.state.persons[2].age} />
-                    </div> : null
-                } 
+                    onClick={this.togglePersonsHandler}>Toggle Person</button>
+                {persons}
+                   
             </div>
         );
     }
