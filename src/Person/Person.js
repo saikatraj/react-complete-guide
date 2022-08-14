874194783934995e0421ed87c766a31a35e472e9
SaikatRaj <saikatrajgaria@gmail.com>
2022-08-14 21:26:54 +0530

    media query andother things done Styling Ended

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Person.css';
+import classes from './Person.css';
 
 
 const person = (props) => 
 {
     return (
-        <div className="Person" style={style}>
+        <div className= {classes.Person} >
             <p onClick = {props.click}>I'm a {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" value={props.name} onChange={props.changed} />
